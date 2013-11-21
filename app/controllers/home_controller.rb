c303e6249d47436f001966de085d21ee7fde8621
Lucas Vieira de Souza <lucasvs@outlook.com>
2013-11-21 20:17:15 -0200

    configure email

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: :token
-  before_filter :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  
 
   def token
   end
--- db/routes.rb
+++ db/routes.rb
@@ -1,59 +1,24 @@
-Blog::Application.routes.draw do
-  resources :articles
+DeviseExample::Application.routes.draw do
 
-  root 'articles#index'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :items
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  resources :checklists
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :tasks
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  resources :colors
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :lists
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  devise_for :users, :admins
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  get '/token' => 'home#token', as: :token
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  resources :home, only: :index
+  resources :admins, only: :index
+  resources :users, only: :index
+
+  root 'home#index'
   
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
